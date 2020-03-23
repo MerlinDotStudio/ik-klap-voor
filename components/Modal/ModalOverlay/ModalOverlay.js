a1d5ccf174594af16a85418a0d560c5d9cd05cd9
meesrutten <meesrutten@gmail.com>
2020-03-23 12:19:03 +0100

    better profanity filter, fix data getting

--- components/Modal/ModalOverlay/ModalOverlay.js
+++ components/Modal/ModalOverlay/ModalOverlay.js
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { ModalOverlay, ModalBoxCSS, ModalBoxGradientCSS } from './_Components';
 import { motion } from 'framer-motion';
 import CloseModalButton from './components/CloseModalButton';
-import Router from 'next/router'
+import Router from 'next/router';
 import { getTotalApplaus, staggerTransition } from '../../../pages';
 import useKeyPress from '../../../utils/useKeypress';
 import firebase from 'firebase';
@@ -13,77 +13,77 @@ export const ModalOverlayContext = React.createContext(undefined);
 export const ModalOverlayContextProvider = props => {
     const [modalOpenState, setModalOpenState] = useState(false);
     const [applausAmount, setApplausAmount] = useState(0);
-	const [options, setOptions] = useState(undefined)
-	const [messages, setMessages] = useState(0)
-	const [clapData, setClapData] = useState(undefined);
+    const [options, setOptions] = useState(undefined);
+    const [messages, setMessages] = useState(0);
+    const [clapData, setClapData] = useState(undefined);
 
-	useEffect( () => {
-		const db = firebase.firestore()
-		const applauseData = db.collection('applaus').get()
-		applauseData.then( snapshot => {
-			const values = []
-			const optionArray = []
-			const fullClapData = [];
+    useEffect(() => {
+        if (!clapData) {
+            const db = firebase.firestore();
+            const applauseData = db.collection('applaus').get();
+            applauseData.then(snapshot => {
+                const values = [];
+                const optionArray = [];
+                const fullClapData = [];
 
-			snapshot.forEach((doc) => {
-				const docData = doc.data()
-				const obj = {
-					value: doc.id,
-					label: docData.name
-				}
-				optionArray.push(obj);
-				values.push(docData.number)
-				fullClapData.push(docData);
-			});
+                snapshot.forEach(doc => {
+                    const docData = doc.data();
+                    const obj = {
+                        value: doc.id,
+                        label: docData.name,
+                    };
+                    optionArray.push(obj);
+                    values.push(docData.number);
+                    fullClapData.push(docData);
+                });
 
-			// const iedereen = db.collection('applaus').doc('iedereen')
-			const sortedClapData = fullClapData.sort((a, b)=>b.number - a.number)
-			const reducer = (accumulator, currentValue) => accumulator + currentValue;
-			setApplausAmount(values.reduce(reducer))
-			setOptions(optionArray)
-			setClapData(sortedClapData);
-		})
+                const sortedClapData = fullClapData.sort((a, b) => b.number - a.number);
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+                setApplausAmount(values.reduce(reducer));
+                setOptions(optionArray);
+                setClapData(sortedClapData);
+            });
 
-		const messageData = db.collection('berichten').get();
-		messageData.then(snapshot => {
-			const dataObject = [];
-			snapshot.forEach(doc => {
-				const docData = doc.data();
-				const messageArray = [];
-				// doc.id = iedereen, artsen, brandweer
-				db.collection('berichten')
-					.doc(doc.id)
-					.collection('messages')
-					.get()
-					.then(data2 => {
-						data2.forEach(item => {
-							const docData2 = item.data();
-							messageArray.push(docData2.messages);
-						});
-						messageArray.forEach(message => {
-							dataObject.push({
-								name: docData.name,
-								message: message.bericht,
-								date: message.date
-							})
-						})
-						// dataObject.push({
-						//     name: docData.name,
-						//     messages: messageArray,
-						// });
-						const sortByNew = dataObject.sort(function(a,b){
-							// Turn your strings into dates, and then subtract them
-							// to get a value that is either negative, positive, or zero.
-							return new Date(b.date) - new Date(a.date);
-						});
-						setMessages(sortByNew);
-					});
-			});
-		});
-		}, [modalOpenState, applausAmount] )
+            const messageData = db.collection('berichten').get();
+            messageData.then(snapshot => {
+                const dataObject = [];
+                snapshot.forEach(doc => {
+                    const docData = doc.data();
+                    const messageArray = [];
+                    // doc.id = iedereen, artsen, brandweer
+                    db.collection('berichten')
+                        .doc(doc.id)
+                        .collection('messages')
+                        .get()
+                        .then(data2 => {
+                            data2.forEach(item => {
+                                const docData2 = item.data();
+                                messageArray.push(docData2.messages);
+                            });
+                            messageArray.forEach(message => {
+                                dataObject.push({
+                                    name: docData.name,
+                                    message: message.bericht,
+                                    date: message.date,
+                                });
+                            });
+                            // dataObject.push({
+                            //     name: docData.name,
+                            //     messages: messageArray,
+                            // });
+                            const sortByNew = dataObject.sort(function(a, b) {
+                                // Turn your strings into dates, and then subtract them
+                                // to get a value that is either negative, positive, or zero.
+                                return new Date(b.date) - new Date(a.date);
+                            });
+                            setMessages(sortByNew);
+                        });
+                });
+            });
+        }
+    }, [modalOpenState, applausAmount]);
 
-
-return (
+    return (
         <ModalOverlayContext.Provider
             value={{
                 isOpen: modalOpenState,
@@ -94,19 +94,19 @@ return (
                 stateChangeHandler: newState => {
                     setModalOpenState(newState);
                     document.body.classList.toggle('has-overlay');
-					if(newState === true) {
-						const audio = new Audio('/sounds/message.mp3');
-						audio.play();
-					}
-					if(newState === false) Router.push('/alle-steun')
-				},
-				updateApplausAmount: newState => {
-					setApplausAmount(newState);
-				},
-				applausAmount: applausAmount,
-				options: options,
-				messageAmount: messages.length,
-				clapData: clapData
+                    if (newState === true) {
+                        const audio = new Audio('/sounds/message.mp3');
+                        audio.play();
+                    }
+                    if (newState === false) Router.push('/alle-steun');
+                },
+                updateApplausAmount: newState => {
+                    setApplausAmount(newState);
+                },
+                applausAmount: applausAmount,
+                options: options,
+                messageAmount: messages.length,
+                clapData: clapData,
             }}
         >
             {props.children}
@@ -118,17 +118,17 @@ export default ({ children, dark = false, closeAsText = false }) => {
     const useModalOverlayContext = useContext(ModalOverlayContext);
     const darkVersion = dark;
 
-	const ModalTransition = staggerTransition
-	ModalTransition.duration = .5
-	ModalTransition.staggerChildren = .25
-	ModalTransition.delayChildren = .25
+    const ModalTransition = staggerTransition;
+    ModalTransition.duration = 0.5;
+    ModalTransition.staggerChildren = 0.25;
+    ModalTransition.delayChildren = 0.25;
 
-	useKeyPress('Escape', () => {
-		if(useModalOverlayContext.isOpen) useModalOverlayContext.stateChangeHandler(false)
-		Router.push('/alle-steun')
-	});
+    useKeyPress('Escape', () => {
+        if (useModalOverlayContext.isOpen) useModalOverlayContext.stateChangeHandler(false);
+        Router.push('/alle-steun');
+    });
 
-	return (
+    return (
         <>
             <ModalOverlay>
                 <motion.div
--- pages/speciaal-bericht.js
+++ pages/speciaal-bericht.js
@@ -24,9 +24,8 @@ import firebase from 'firebase';
 
 const SpecialMessage = () => {
     const useModalOverlayContext = useContext(ModalOverlayContext);
-    const [textLength, setTextLength] = useState(0);
+    const [textValue, setTextValue] = useState(0);
     const [selectValue, setSelectValue] = useState(useModalOverlayContext.options && useModalOverlayContext.options[0]);
-    const TextRef = useRef(undefined);
     const db = firebase.firestore();
 
     return (
@@ -98,10 +97,8 @@ const SpecialMessage = () => {
                                     placeholder={'Deel hier jouw dank en persoonlijke bericht…'}
                                     required={true}
                                     maxLength={280}
-                                    onInput={e => {
-                                        setTextLength(e.target.value.length);
-                                    }}
-                                    ref={TextRef}
+									defaultValue={''}
+                                    onInput={e => setTextValue(e.target.value)}
                                 />
                                 <span
                                     css={css`
@@ -109,10 +106,10 @@ const SpecialMessage = () => {
                                         bottom: 1.45rem;
                                         right: 0.75rem;
                                         font-size: 0.825rem;
-                                        color: ${textLength < 8 ? theme.colors.statusRed : '#333'};
+                                        color: ${textValue.length < 8 ? theme.colors.statusRed : '#333'};
                                     `}
                                 >
-                                    {textLength} / 280
+                                    {textValue.length ? textValue.length : 0} / 280
                                 </span>
                             </div>
                             <BottomPosition>
@@ -121,20 +118,19 @@ const SpecialMessage = () => {
                                     <Button
                                         key={2}
                                         icon={'💌'}
-                                        ariaDisabled={textLength < 8}
+                                        ariaDisabled={textValue.length < 8}
                                         onClick={() => {
-                                            if (textLength < 8) return;
-											const regex = new RegExp(/aapmens|aarstulp|adder|addergebroed|apenkont|appelflap|autist|babok|badgast|baggerduiker|bangerd|barslet|bedrijfspoedel|beheime|belhamel|bengel|bermslet|bitch|bloedlijer|boer|boerenheikneuter|boerenhufter|boerenkaffer|boerenkarhengst|boerenkinkel|boerenpummel|boerin|bokkenrijder|bosneuker|botterik|bouwdoos|breezerslet|brilsmurf|bruinwerker|bulderbast|charlatan|choco|chocoladehoer|chocoladesnol|cijferneuker|del|dirkdoos|doos|dreuzel|droplul|druiloor|dwaas|eendenkont|eikel|etter|etterbuil|ezel|fielt|flapdrol|fleer|flessentrekker|flikker|galgenbrok|gangster|gannef|geit|geitenbreier|geitenneuker|geteisem|gluiper|gluiperd|gluipsnor|gratenbaal|greppeldel|grobbejanus|haai|halvezool|hapsnurker|harpij|heihaas|heikneuter|heks|hersenlijer|hockeytrut|hockeytut|hoerenjager|hoerenjong|hoerenkind|hoerenzoon|homo|hondenlul|hondenneuker|hufter|huppelkut|huzarenhoop|kaas|kaashoer|kaaskop|kaffer|kakhuis|kakmadam|kakmaker|kamelenneuker|kamerolifant|kanker|kankerhoer|kankerhond|kankerlijder|kankerlijer|kankernicht|kapsoneslijer|karhengst|karonje|kees|keilef|kelerelijer|keutelkut|kinkel|kip|kippenneuker|klafte|klapkut|klaplul|klapperaap|klere|kloefkapper|klojo|klooi|kloothannes|kloothommel|klootviool|klootzak|klote|kluns|knijpkont|koeskoesvreter|koffieboon|kokosmakroon|kontkruiper|kontneuker|kreng|kriel|krielkip|krijslijster|kruimelbuik|kruiper|kut|kutstreek|kuttenkop|kutvent|kutwijf|kwakzalver|labbekak|labrat|lafbek|lamstraal|lelijkerd|linkmiegel|lomperd|lomperik|loser|luibak|luibuis|luiskop|lul|lulhannes|lummel|macaronivreter|maffer|mafketel|mafkikker|makaak|mietje|mispruim|mispunt|mof|moffenhoer|Moffrika|mongool|morsebel|mosselhoer|muilezelin|muts|natkut|netenvreter|neukpaal|nitwit|noppeshoer|nul|oen|olijfneuker|onderkruipsel|optyfen|ossenkop|ossenlul|ouwehoedendoos|pagadder|palurk|paplap|pasjakroet|patjakker|pedo|pekelhoer|pepermuntvreter|pestlijder|pigmentvreter|pinda|pindachinees|pindapoepchinees|pleurislijder|ploert|plucheplakker|plurk|pokkenlijder|pokkenwijf|polak|politiemuts|pommel|populist|pothoer|puistenkop|pukkelbek|pummel|randdebiel|reetkever|reetveger|rekel|roetmop|rotmof|rotzak|rugridder|ruigpoot|sakkers|sambalburger|sambalvreter|schapenneuker|schelm|schijterd|schijtlaars|schijtlijster|schijtluis|schimmelkut|schobbejak|schoft|schurftkop|schurk|secreet|sekreet|slet|slettenbak|slijmerd|sloerie|smeerkanis|smeerlap|smeerpijp|smiecht|smous|snoever|snotaap|snotolf|sodemieter|sodomiet|soepkip|spaghettivreter|spast|steenezel|stinker|stinkerd|stoephoer|stomkop|strontzak|taalnazi|taart|teef|teringlijder|teringlijer|tietvlieg|totebel|trut|tyfuslijder|variétéhoer|varken|vetklep|vetzak|vlooienzak|vuilak|vullis|wipkip|zaagselkop|zakkenvuller|zakkenwasser|zalfpot|zeikbeer|zeikerd|zeikhannes|zeiklijster|zeiksnor|zeikstraal|zenuwenlijer|zenuwlijer|zeur|zeurkous|zeurpiet|zeurzak|zwijn|zwijnjak|kech|hoer|kkr|kut|slet|mongool|jood|joden|nazi|geil|neuken|pik|lul/)
-											if(regex.test(TextRef.current.value)){
-												alert('Het lijkt er op dat er scheldwoorden in je tekst staan, haal deze even weg aub!')
-												return
+                                            if (textValue.length < 8) return;
+											const regex = new RegExp(/\W(aapmens|aarstulp|adder|addergebroed|apenkont|appelflap|autist|babok|badgast|baggerduiker|bangerd|barslet|bedrijfspoedel|beheime|belhamel|bengel|bermslet|bitch|bloedlijer|boer|boerenheikneuter|boerenhufter|boerenkaffer|boerenkarhengst|boerenkinkel|boerenpummel|boerin|bokkenrijder|bosneuker|botterik|bouwdoos|breezerslet|brilsmurf|bruinwerker|bulderbast|charlatan|choco|chocoladehoer|chocoladesnol|cijferneuker|del|dirkdoos|doos|dreuzel|droplul|druiloor|dwaas|eendenkont|eikel|etter|etterbuil|ezel|fielt|flapdrol|fleer|flessentrekker|flikker|galgenbrok|gangster|gannef|geit|geitenbreier|geitenneuker|geteisem|gluiper|gluiperd|gluipsnor|gratenbaal|greppeldel|grobbejanus|haai|halvezool|hapsnurker|harpij|heihaas|heikneuter|heks|hersenlijer|hockeytrut|hockeytut|hoerenjager|hoerenjong|hoerenkind|hoerenzoon|homo|hondenlul|hondenneuker|hufter|huppelkut|huzarenhoop|kaas|kaashoer|kaaskop|kaffer|kakhuis|kakmadam|kakmaker|kamelenneuker|kamerolifant|kanker|kankerhoer|kankerhond|kankerlijder|kankerlijer|kankernicht|kapsoneslijer|karhengst|karonje|kees|keilef|kelerelijer|keutelkut|kinkel|kip|kippenneuker|klafte|klapkut|klaplul|klapperaap|klere|kloefkapper|klojo|klooi|kloothannes|kloothommel|klootviool|klootzak|klote|kluns|knijpkont|koeskoesvreter|koffieboon|kokosmakroon|kontkruiper|kontneuker|kreng|kriel|krielkip|krijslijster|kruimelbuik|kruiper|kut|kutstreek|kuttenkop|kutvent|kutwijf|kwakzalver|labbekak|labrat|lafbek|lamstraal|lelijkerd|linkmiegel|lomperd|lomperik|loser|luibak|luibuis|luiskop|lul|lulhannes|lummel|macaronivreter|maffer|mafketel|mafkikker|makaak|mietje|mispruim|mispunt|mof|moffenhoer|Moffrika|mongool|morsebel|mosselhoer|muilezelin|muts|natkut|netenvreter|neukpaal|nitwit|noppeshoer|olijfneuker|onderkruipsel|optyfen|ossenkop|ossenlul|ouwehoedendoos|pagadder|palurk|paplap|pasjakroet|patjakker|pedo|pekelhoer|pepermuntvreter|pestlijder|pigmentvreter|pinda|pindachinees|pindapoepchinees|pleurislijder|ploert|plucheplakker|plurk|pokkenlijder|pokkenwijf|polak|politiemuts|pommel|populist|pothoer|puistenkop|pukkelbek|pummel|randdebiel|reetkever|reetveger|rekel|roetmop|rotmof|rotzak|rugridder|ruigpoot|sakkers|sambalburger|sambalvreter|schapenneuker|schelm|schijterd|schijtlaars|schijtlijster|schijtluis|schimmelkut|schobbejak|schoft|schurftkop|schurk|secreet|sekreet|slet|slettenbak|slijmerd|sloerie|smeerkanis|smeerlap|smeerpijp|smiecht|smous|snoever|snotaap|snotolf|sodemieter|sodomiet|soepkip|spaghettivreter|spast|steenezel|stinker|stinkerd|stoephoer|stomkop|strontzak|taalnazi|taart|teef|teringlijder|teringlijer|tietvlieg|totebel|trut|tyfuslijder|variétéhoer|varken|vetklep|vetzak|vlooienzak|vuilak|vullis|wipkip|zaagselkop|zakkenvuller|zakkenwasser|zalfpot|zeikbeer|zeikerd|zeikhannes|zeiklijster|zeiksnor|zeikstraal|zenuwenlijer|zenuwlijer|zeur|zeurkous|zeurpiet|zeurzak|zwijn|zwijnjak|kech|hoer|kkr|kut|slet|mongool|jood|joden|nazi|geil|neuken|pik|lul)\W/)
+											if(regex.test(textValue)){
+												alert('Het lijkt er op dat er scheldwoorden in je tekst staan, haal deze even weg aub!');
 											} else {
 												db.collection('berichten')
 													.doc(selectValue.value)
 													.collection('messages')
 													.add({
 														messages: {
-															bericht: TextRef.current.value,
+															bericht: textValue,
 															date: Date.now(),
 														},
 													});
